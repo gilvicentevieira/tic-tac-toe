9216d082773e86e64a6b023677ba5a927647e5f0
Gil <gilvicentevieira@gmail.com>
2024-01-30 05:28:44 +0000

    GameBoard comp refactor

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,37 +1,12 @@
-const initialGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
-
-const GameBoard = ({onSelectSquare, turns}) => {
-    let gameBoard = initialGameBoard
-
-    for(const turn of turns){
-        const {rowIndex, colIndex} = turn.square
-        gameBoard[rowIndex][colIndex] = turn.player
-    }
-
-//   const [gameBoard, setGameBoard] = useState(initialGameBoard);
-
-//   const handleSelectSquare = (rowIndex, colIndex) => {
-//     setGameBoard((prev) => {
-//       const updatedGameBoard = [...gameBoard.map((innerArr) => [...innerArr])];
-//       updatedGameBoard[rowIndex][colIndex] = activePlayer;
-//       return updatedGameBoard;
-//     });
-
-//     onSelectSquare()
-//   };
-
+const GameBoard = ({onSelectSquare, board}) => {
   return (
     <ol id="game-board">
-      {gameBoard.map((row, rowIndex) => (
+      {board.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={()=>onSelectSquare(rowIndex,colIndex)}>
+                <button onClick={()=>onSelectSquare(rowIndex,colIndex)} disabled={playerSymbol !== null}>
                   {playerSymbol}
                 </button>
               </li>
