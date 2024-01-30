6d30e53620afda77d0a0aad584e6a8456dee5aa2
Gil <gilvicentevieira@gmail.com>
2024-01-30 03:06:02 +0000

    added gameboard comp impl

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,9 +1,41 @@
-const GameBoard = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+import { useState } from "react";
 
-export default GameBoard
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+const GameBoard = ({onSelectSquare, activePlayer}) => {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  const handleSelectSquare = (rowIndex, colIndex) => {
+    setGameBoard((prev) => {
+      const updatedGameBoard = [...gameBoard.map((innerArr) => [...innerArr])];
+      updatedGameBoard[rowIndex][colIndex] = activePlayer;
+      return updatedGameBoard;
+    });
+
+    onSelectSquare()
+  };
+
+  return (
+    <ol id="game-board">
+      {gameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+};
+
+export default GameBoard;
